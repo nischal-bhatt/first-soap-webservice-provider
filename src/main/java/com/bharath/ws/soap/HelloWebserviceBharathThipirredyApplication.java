9b4bcd8fc8f9f0f83e62b197a9c373f84fa1599f
nischal-bhatt <nischal.bhatt.87@gmail.com>
2022-05-26 22:43:10 +0800

    first soap web service

---
+++ src/main/java/com/bharath/ws/soap/HelloWebserviceBharathThipirredyApplication.java
@@ -0,0 +1,13 @@
+package com.bharath.ws.soap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWebserviceBharathThipirredyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWebserviceBharathThipirredyApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/bharath/ws/soap/HelloWs.java
@@ -0,0 +1,19 @@
+package com.bharath.ws.soap;
+
+import javax.jws.WebMethod;
+import javax.jws.WebService;
+
+import org.apache.cxf.feature.Features;
+
+@WebService
+@Features(features="org.apache.cxf.feature.LoggingFeature")
+//this is from jaxws api
+public class HelloWs {
+
+	
+	@WebMethod
+	public String hello()
+	{
+		return "hello";
+	}
+}
